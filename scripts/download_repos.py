118c20e13b8265c170fc7d6a447cdcb2527ffb2a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-13 05:22:04 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/download_repos.py
+++ scripts/download_repos.py
@@ -103,7 +103,26 @@ class RepoDownloader:
 
     def _extract_stream(self, stream, extract_dir):
         with tarfile.open(fileobj=stream, mode='r|*') as tar:
-            tar.extractall(path=extract_dir, members=self._included_files(tar, self._file_whitelist))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=extract_dir, members=self._included_files(tar,self._file_whitelist))
 
     @staticmethod
     def _get_extracted_repo_path(output_dir, repo_name):
